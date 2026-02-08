bf5b17075f3a6c6bad267348461d1e10928fa002
juhanjarif <ahmadjarif2004@gmail.com>
2026-02-08 16:21:04 +0600

    add: added authMiddleware & update:  sandController

--- backend/src/controllers/sandController.js
+++ backend/src/controllers/sandController.js
@@ -1,3 +1,27 @@
-export const saveCanvas = (req, res) => {
-    res.json({ message: "Canvas saved" });
+import Sand from "../models/sandModel.js"
+
+export const saveCanvas = async (req, res) => {
+    try{
+        const { imagePath } = req.body;
+
+        if(!imagePath){
+            return res.status(400).json({
+                message: "Image Path required!"
+            });
+        }
+
+        const sand = await Sand.create({
+            userID: req.user._id,
+            imagePath
+        });
+
+        res.status(201).json({
+            message: "Canvas Saved", sand
+        });
+    } catch(error){
+        console.error("SandCanvas Error: ", error);
+        res.status(500).json({
+            message: "Server Error"
+        });
+    }
 };

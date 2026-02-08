d9572a3254344b698971d600887a4bfd7e32715d
Tamajose <tnvrmh41@gmail.com>
2026-02-09 02:45:22 +0600

    canvas routing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    server: {
-        open: true,
-    }    
-})
+  server: {
+    open: true,
+  },
+  input: {
+    main: "index.html",
+    canvas: "canvas.html",
+    signin: "signin.html",
+    signup: "signup.html",
+  },
+});

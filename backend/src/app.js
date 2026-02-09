fb85bdc24fcfa89496f69d0acd3546b26865b5db
Tamajose <tnvrmh41@gmail.com>
2026-02-09 23:29:56 +0600

    added multer middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
 //Routes
 app.use("/api/creations", sandRoutes);
@@ -19,7 +20,7 @@ app.use("/api/auth", authRoutes);
 
 //Check
 app.get("/check", (req, res) => {
-    res.json({ message: "Server is running" });
+  res.json({ message: "Server is running" });
 });
 
 export default app;

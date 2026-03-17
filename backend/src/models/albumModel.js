20322d96be697e31c1957c1dcae1bd7725f04c0d
juhanjarif <ahmadjarif2004@gmail.com>
2026-03-17 17:38:56 +0600

    add: added the model for album

---
+++ backend/src/models/albumModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const albumSchema = new mongoose.Schema({
+        userID: {
+            type: mongoose.Schema.Types.Array.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        images: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Sand",
+            },
+        ],
+    },
+    { timestamps: true }
+);
+
+export default mongoose.model("Album", albumSchema);
